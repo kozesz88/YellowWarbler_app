fee8dc2f6a48718324b3260d455a678f0238b76c
kozesz88 <39737018+kozesz88@users.noreply.github.com>
2020-04-03 20:42:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/singlescreenapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.singlescreenapp;
+//This project was made by using the pictures and the information from https://www.instagram.com/yellowwarblercoffee/ //
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
